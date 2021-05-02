c014641eaa6d915b3219f1e1a3a01a3edc3aecef
Jisu <jisuc@credfin.io>
2021-05-02 14:29:45 +1000

    Wrapped App component with Router

--- crwn-clothing/src/components/menu-item/menu-item.component.jsx
+++ crwn-clothing/src/components/menu-item/menu-item.component.jsx
@@ -1,18 +1,28 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
-import './menu-item.styles.scss';
+import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+  console.log("history:", history);
+  console.log("match:", match);
+
+
+  return (
     <div
-      className="background-image"
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    />
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">SHOP NOW</span>
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
-export default MenuItem;
+export default withRouter(MenuItem);
--- crwn-clothing/src/pages/homepage.component.jsx
+++ crwn-clothing/src/pages/homepage.component.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import Directory from '';
+import Directory from '../components/directory/directory.component';
 
-import './homepage.styles.sass'
+import './homepage.styles.scss'
 
-const HomePage = () => {
+const HomePage = () => (
   <div className='homepage'>
     <Directory />
   </div>
-}
+)
 
 export default HomePage;
